4c43dde7d8fdb1717736377da26937f6654914c3
Ryan McGowan <ryan@ryanmcg.com>
2014-02-15 13:57:27 -0800

    Inc version to 0.1.0.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject incise-stefon "0.1.0-SNAPSHOT"
+(defproject incise-stefon "0.1.0"
   :description "An couple of extensions to incise adding support for stefon."
   :url "https://github.com/RyanMcG/incise-stefon"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[incise-core "0.2.0-SNAPSHOT"]
+  :dependencies [[incise-core "0.2.0"]
                  [com.ryanmcg/stefon "0.5.0-bdfb91935e8d91f1b2b1991f2af2ce2fca173490"]])
